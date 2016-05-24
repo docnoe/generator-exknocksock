ac14f4e484ef232170f9f2b88216ff1b9e369e39
Johannes Noe <noe@ferrari-electronic.de>
2016-05-24 09:04:45 +0200

    feature(component): compenent can be created in sub directory
    a proper component name is generated without slashes

--- component/index.js
+++ component/index.js
@@ -3,6 +3,7 @@ var util = require('util');
 var yeoman = require('yeoman-generator');
 var fs = require('fs');
 var chalk = require('chalk');
+var path = require('path');
 
 var ComponentGenerator = yeoman.generators.NamedBase.extend({
 
@@ -23,23 +24,24 @@ var ComponentGenerator = yeoman.generators.NamedBase.extend({
     init: function() {
         var codeLanguage = this.usesCoffeeScript ? 'CoffeeScript' : 'JavaScript';
         console.log('Creating component \'' + this.name + '\' (' + codeLanguage + ')...');
-        this.componentName = this.name;
-        this.dirname = this.sourceBase + 'components/' + this._.dasherize(this.name) + '/';
         this.filename = this._.dasherize(this.name);
+        this.componentName = path.basename(this.filename);
+        this.registeredName = this.filename.replace(/\//g, "-");
+        this.dirname = this.sourceBase + 'components/' + this.filename + '/';
         this.viewModelClassName = this._.classify(this.name);
     },
 
     template: function() {
-        this.copy('view.html', this.dirname + this.filename + '.html');
-        this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.filename + this.codeFileExtension);
+        this.copy('view.html', this.dirname + this.componentName + '.html');
+        this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.componentName + this.codeFileExtension);
     },
 
     addComponentRegistration: function() {
         var startupFile = this.sourceBase + 'app/startup' + this.codeFileExtension;
         readIfFileExists.call(this, startupFile, function(existingContents) {
-            var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
+            var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.registeredName + '[\'"]');
             if (existingRegistrationRegex.exec(existingContents)) {
-                this.log(chalk.white(this.filename) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
+                this.log(chalk.white(this.registeredName) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
                 return;
             }
             var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]';
@@ -47,11 +49,11 @@ var ComponentGenerator = yeoman.generators.NamedBase.extend({
                 token = '# [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]'
             };
             var regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
-                modulePath = 'components/' + this.filename + '/' + this.filename,
-                lineToAdd = this.usesCoffeeScript ? 'ko.components.register \"' + this.filename + '\",\n    require: \'' + modulePath + '\'' : 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
+                modulePath = 'components/' + this.filename + '/' + this.componentName,
+                lineToAdd = this.usesCoffeeScript ? 'ko.components.register \"' + this.registeredName + '\",\n    require: \'' + modulePath + '\'' : 'ko.components.register(\'' + this.registeredName + '\', { require: \'' + modulePath + '\' });',
                 newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
             fs.writeFile(startupFile, newContents);
-            this.log(chalk.green('   registered ') + chalk.white(this.filename) + chalk.green(' in ') + chalk.white(startupFile));
+            this.log(chalk.green('   registered ') + chalk.white(this.registeredName) + chalk.green(' in ') + chalk.white(startupFile));
 
             if (this.usesCoffeeScript) {
                 if (fs.existsSync('gulpfile.coffee')) {
--- component/templates/viewmodel.coffee
+++ component/templates/viewmodel.coffee
@@ -1,11 +1,11 @@
 define [
   "knockout"
-  "text!./<%= filename %>.html"
+  "text!./<%= componentName %>.html"
 ], (ko, templateMarkup) ->
 
   class <%= viewModelClassName %>
     constructor: (params) ->
-      @message = ko.observable("Hello from the <%= name %> component!")
+      @message = ko.observable("Hello from the <%= registeredName %> component!")
       # This runs when the component is torn down. Put here any logic necessary to clean up,
       # for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
       @dispose = ->
